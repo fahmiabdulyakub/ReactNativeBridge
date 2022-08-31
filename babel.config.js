64be0a87432b195b28221a5c6f7db861586e9d43
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-08-31 12:06:21 +0700

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@assets': './src/assets',
+          '@screens': './src/screens',
+          '@themes': './src/themes',
+          '@types': './src/types',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
+};
