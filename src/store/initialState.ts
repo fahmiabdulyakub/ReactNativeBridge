4476e6b62895e00150abbd9f9182e5ee0c071d26
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-09-03 00:40:15 +0700

    create initialstate

---
+++ src/store/initialState.ts
@@ -0,0 +1,10 @@
+import {GlobalType} from 'types/GlobalType';
+
+const initialstate: GlobalType = {
+  cart: {
+    cartList: [],
+    cartTotalQuantity: 0,
+  },
+};
+
+export default initialstate;
